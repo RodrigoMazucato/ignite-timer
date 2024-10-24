c617f30a1cc83f00bef6f40d8135b6e5a3c979af
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2024-10-24 13:08:21 -0300

    style: definindo os estilos globais da aplicação

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,4 +1,20 @@
 export const defaultTheme = {
-  primary: 'white',
-  secondary: 'red',
+  white: '#FFF',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'yellow-500': '#FBA94C',
 }
